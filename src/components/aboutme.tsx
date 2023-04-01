61b231673e79ffc02247d7a707f7526729ff4b23
Matija Benko <matijabenko@yahoo.com>
2023-03-31 18:02:14 -0700

    XS screensize integrated with Aboutme Component

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -5,7 +5,10 @@ function AboutMe(): JSX.Element {
     return (
         <>
             <div id="aboutme" className=" pt-20 w-screen border-y-4 border-black bg-gradient-to-r from-indigo-800 via-purple-700 to-pink-600">
-                <div className="container mx-auto p-10 flex items-center justify-center sm:flex-col sm:p-0 xs:flex-col xs:px-8">
+                <div className="container mx-auto p-10 flex items-center justify-center 
+                sm:flex-col sm:p-0 
+                xs:flex-col xs:px-8
+                2xs:flex-col 2xs:px-8">
                     <div className="w-1/2">
                         <img
                             src={Logo}
@@ -13,9 +16,22 @@ function AboutMe(): JSX.Element {
                             className="rounded-full border-2"
                         />
                     </div>
-                    <div className="w-1/2 p-10 sm:w-full xs:w-full xs:p-0">
-                        <h1 className="text-8xl text-white font-bold mb-3 lg:text-7xl md:text-5xl sm:text-5xl sm:text-center xs:text-center xs:text-4xl xs:pt-5">{"About Me"}</h1>
-                        <p className='text-white font-bold text-xl py-4 max-w-lg md:text-lg sm:text-base xs:text-sm'>
+                    <div className="w-1/2 p-10 
+                    sm:w-full 
+                    xs:w-full xs:p-0
+                    2xs:w-full 2xs:p-0">
+                        <h1 className="text-8xl text-white font-bold mb-3 
+                        lg:text-7xl 
+                        md:text-5xl 
+                        sm:text-5xl 
+                        sm:text-center 
+                        xs:text-center xs:text-4xl xs:pt-5
+                        2xs:text-center 2xs:text-xl 2xs:pt-5">{"About Me"}</h1>
+                        <p className='text-white font-bold text-xl py-4 max-w-lg 
+                        md:text-lg 
+                        sm:text-base 
+                        xs:text-sm
+                        2xs:text-xs'>
                             {"I absolutely love programming and especially game development. My favorite projects involve developing 3D and VR games in Unity, while I also enjoy dabbling in web development / creating assets on blender on the side. I'm always continuously trying to learn and expand my skill set."}
                         </p>
                     </div>
